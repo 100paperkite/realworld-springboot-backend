fdbd613a0e67d29f622513385a289efb3c0fe2f2
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-14 17:44:30 +0900

    refactor: replace custom `UserRepository` to `JpaRepository`

--- src/main/java/com/realworld/backend/repository/UserRepository.java
+++ src/main/java/com/realworld/backend/repository/UserRepository.java
@@ -1,31 +1,12 @@
 package com.realworld.backend.repository;
 
 import com.realworld.backend.domain.User;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class UserRepository {
-    private final EntityManager em;
-
-    public Long save(User user) {
-        em.persist(user);
-        return user.getId();
-    }
-
-    public User find(Long id) {
-        return em.find(User.class, id);
-    }
-
-    public Optional<User> findByName(String name) {
-        return em.createQuery("select user from User user where user.name=:name", User.class)
-                .setParameter("name", name)
-                .getResultList()
-                .stream()
-                .findFirst();
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
 }
