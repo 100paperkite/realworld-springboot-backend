4c9f219bb3c3eff85d579186146843b4252ed087
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-14 22:10:51 +0900

    feat: add login service and test

--- src/main/java/com/realworld/backend/service/UserService.java
+++ src/main/java/com/realworld/backend/service/UserService.java
@@ -1,12 +1,14 @@
 package com.realworld.backend.service;
 
 import com.realworld.backend.domain.User;
-import com.realworld.backend.exception.RealWorldError;
 import com.realworld.backend.exception.RealWorldException;
 import com.realworld.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.realworld.backend.exception.RealWorldError.AUTHENTICATION_FAILED;
+import static com.realworld.backend.exception.RealWorldError.DUPLICATE_USER;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +25,18 @@ public class UserService {
     private void validateDuplicated(User user) {
         userRepository.findByEmail(user.getEmail())
                 .ifPresent((u) -> {
-                    throw new RealWorldException(RealWorldError.DUPLICATE_USER);
+                    throw new RealWorldException(DUPLICATE_USER);
                 });
+    }
+
+    @Transactional(readOnly = true)
+    public User login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RealWorldException(AUTHENTICATION_FAILED));
+
+        if (!user.getPassword().equals(password)) {
+            throw new RealWorldException(AUTHENTICATION_FAILED);
+        }
+        return user;
     }
 }
--- src/test/java/com/realworld/backend/service/UserServiceTest.java
+++ src/test/java/com/realworld/backend/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static com.realworld.backend.exception.RealWorldError.AUTHENTICATION_FAILED;
 import static com.realworld.backend.exception.RealWorldError.DUPLICATE_USER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,6 +19,51 @@ class UserServiceTest {
 
     private final UserRepository userRepository = mock(UserRepository.class);
     private final UserService userService = new UserService(userRepository);
+
+
+    @Test
+    @DisplayName("로그인에 성공한다")
+    public void assertLoginSucceed() throws Exception {
+        //given
+        User user = new User("test", "test@test.test", "test");
+
+        given(userRepository.findByEmail(user.getEmail()))
+                .willReturn(Optional.of(user));
+
+        //when
+        User expected = userService.login(user.getEmail(), user.getPassword());
+
+        //then
+        assertThat(expected).isEqualTo(user);
+    }
+
+    @Test
+    @DisplayName("로그인할 유저가 존재하지 않으면 예외가 난다")
+    public void assertExceptionWhenLoginUserNotExist() throws Exception {
+        //given
+        String email = "test@test.test";
+        given(userRepository.findByEmail(email))
+                .willReturn(Optional.empty());
+
+        //when, then
+        assertThatThrownBy(() -> userService.login(email, "invalid"))
+                .isInstanceOf(RealWorldException.class)
+                .hasMessage(AUTHENTICATION_FAILED.message());
+    }
+
+    @Test
+    @DisplayName("로그인 비밀번호가 맞지 않으면 예외가 난다")
+    public void assertExceptionWhenLoginFailed() throws Exception {
+        //given
+        User user = new User("test", "test@test.test", "test");
+        given(userRepository.findByEmail(user.getEmail()))
+                .willReturn(Optional.of(user));
+
+        //when, then
+        assertThatThrownBy(() -> userService.login(user.getEmail(), "invalid"))
+                .isInstanceOf(RealWorldException.class)
+                .hasMessage(AUTHENTICATION_FAILED.message());
+    }
 
     @Test
     @DisplayName("중복 유저라면 예외 발생")
