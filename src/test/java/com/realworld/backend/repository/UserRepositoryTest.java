df3cedf9e48b459e32e19a92c44b7840b2c59f32
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-14 18:12:51 +0900

    test: add a test of user repository

---
+++ src/test/java/com/realworld/backend/repository/UserRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.realworld.backend.repository;
+
+import com.realworld.backend.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @Transactional
+    void assertFindByName() {
+        User user = new User("test", "test@test.test", "testtesttest");
+        userRepository.save(user);
+
+        Optional<User> found = userRepository.findByName("test");
+        Optional<User> notFound = userRepository.findByName("notFound");
+
+        assertThat(found).isPresent();
+        assertThat(notFound).isNotPresent();
+
+        assertThat(found.get()).isEqualTo(user);
+    }
+}
